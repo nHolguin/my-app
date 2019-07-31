ad8b6042197f21401b1af4f827a414b6215ae773
Nimrod Holguin <nimrodholguin@gmail.com>
2019-07-31 02:38:07 -0400

    Primer Commit Angular - Formulario.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  title = 'Formulario';
+  contacto: FormGroup;
+  submitted = false;
+  titulo = 'Crear un Formulario con Angular 7 y Bootstrap 4 + Validación';
+  constructor(private formBuilder: FormBuilder) { }
+ 
+    ngOnInit() {
+        this.contacto = this.formBuilder.group({
+            nya: ['', Validators.required],            
+            email: ['', [Validators.required, Validators.email]],
+            asunto: ['', Validators.required],
+            postre: ['', Validators.required],
+            mensaje: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+ 
+    get f() { return this.contacto.controls; }
+ 
+    onSubmit() {
+        this.submitted = true;
+ 
+        if (this.contacto.invalid) {
+            return;
+        }
+ 
+        alert('Mensaje Enviado !')
+      }
 }
